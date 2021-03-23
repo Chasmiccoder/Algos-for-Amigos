066597c2b1b2bc713e1570184e9358079d02570d
Chasmic Coder <chasmiccoder@gmail.com>
2021-03-23 12:28:14 +0530

    Added Comment lines to Linked list Implementation

--- Computer Science/DSA Fundamentals/p14_linked_list_implementation.cpp
+++ Computer Science/DSA Fundamentals/p14_linked_list_implementation.cpp
@@ -17,6 +17,10 @@ void print_instructions();
 
 
 int length_linked_list( Node* head_ptr ) {
+    /*
+    Returns the length of the linked list
+    */
+    
     int len = 0;
     Node* current = new Node;
     current = head_ptr;
@@ -30,6 +34,12 @@ int length_linked_list( Node* head_ptr ) {
 
 
 void insert( Node* head_ptr, string element, int position = -1 ) {
+    /*
+    Inserts an element at the specified position in the linked list
+    If the position is -1, the element gets inserted at the end
+    */
+    
+    // Check if the position is valid
     int length = length_linked_list( head_ptr );
     if ( position < -1 || position > length ) {
         printf( "Invalid Position!\n\n" );
@@ -52,14 +62,18 @@ void insert( Node* head_ptr, string element, int position = -1 ) {
         Node* current;
         current = head_ptr;
 
+        // Traverse to the last node of the list
         while ( (*current).link != NULL ) {
             current = (*current).link;
         }
 
+        // Link the node created to the last element
         (*current).link = temp;
     }
 
+    // To insert an element in the first position, update the head to point to the new node, and the new node to point to the first node
     else if ( position == 0 ) {
+        
         
         Node* temp = new Node;
     
@@ -76,23 +90,29 @@ void insert( Node* head_ptr, string element, int position = -1 ) {
 
         current = head_ptr;
 
-        int i = 0;
+        // Traverse all the way to the i-1th node (we make i iterations, but start from the head, which means that end at the (i-1)th node)
         for ( int i = 0; i < position; i++ ) {
             current = current->link;
         }
 
+        // Link the new node to the ith node. Now the ith node becomes the (i+1)th node
         temp->link = current->link;
+        // Link the (i-1)th node to the new node. Now the new node becomes the ith node
         current->link = temp;
     }
 }
 
+
 void delete_element( Node* head_ptr, int position ) {
+
+    // Check if the position is valid
     int length = length_linked_list( head_ptr );
     if ( position < -1 || position >= length ) {
         printf( "Invalid Position!\n\n" );
         return;
     }
 
+    // If we want to delete the first element, update the head to point to the 2nd node (node number 1)
     if ( position == 0 ) {
         Node* current = new Node;
         current = head_ptr;
@@ -101,38 +121,46 @@ void delete_element( Node* head_ptr, int position ) {
         head_ptr->data = "";
     }
 
+    // If position is -1 (default argument), delete the last element
     else if ( position == -1 || position == length - 1 ) {
         Node* current = new Node;
         Node* previous = new Node;
         current = head_ptr;
         previous = head_ptr;
 
+        // Traverse to the last node
         while ( current->link ) {
             previous = current;
             current = current->link;
         }
+        // Update the (n-1)th node to point to a NULL value
         previous->link = NULL;
     }
 
     else {
+
         Node* current = new Node;
         Node* previous = new Node;
         current = head_ptr;
         previous = head_ptr;
         int i = 0;
+
+        // Traverse to the ith element
         for ( int i = 0; i <= position; i++ ) {
             previous = current;
             current = current->link;
         }
+        // The (i-1)th element will point to the (i+1)th element
         previous->link = current->link;
-
     }
-
-    
 }
 
 
 void print_linked_list( Node* head_ptr ) {
+    /* 
+    Function to print the entire linked list
+    */
+    
     Node* current = new Node;
     current = head_ptr;
     
@@ -152,6 +180,7 @@ void print_linked_list( Node* head_ptr ) {
     cout << "-Tail-" << endl;
 }
 
+
 void print_menu() {
     printf( "Operations:\n" );
     printf( "===========\n");
@@ -161,8 +190,8 @@ void print_menu() {
     printf( "4 - Print Length of Linked List\n" );
     printf( "0 - Terminate the program\n" );
     printf( "\n" );
-
 }
+
 
 void print_instructions() {
     printf( "\nInstructions:\n" );
