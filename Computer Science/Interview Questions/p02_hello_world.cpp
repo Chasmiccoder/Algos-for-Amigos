da56a19b782e218f017189dbf576c7203f06595c
Chasmic Coder <chasmiccoder@gmail.com>
2021-03-26 14:20:43 +0530

    Added hello world to Interview Qs

---
+++ Computer Science/Interview Questions/p02_hello_world.cpp
@@ -0,0 +1,12 @@
+/*
+print "Hello World"...
+(Without using a semicolon in the program ';' )
+*/
+
+#include <iostream>
+int main() {
+
+    if ( printf( "Hello World!\n" ) == 0 ) {
+        
+    }
+}
