77ad8e7336f2ca1dd2bd236c35ac3a931806ec98
Aryaman Kolhe <chasmiccoder@gmail.com>
2021-08-12 19:05:41 +0530

    Added gitignore to CS folder

--- Computer_Science/2_Competitive_Programming/Codeforces/800_rating/p10_Cherry.c
+++ Computer_Science/2_Competitive_Programming/Codeforces/800_rating/p10_Cherry.c
@@ -20,8 +20,31 @@ int main() {
         int arr[n];
 
         for ( int i = 0; i < n; i++ ) {
-            
+            scanf("%d", &arr[i]);
         }
+
+        long long maxResult = 0;
+        for ( int l = 0; l < n; l++ ) {
+            for ( int r = l+1; r < n; r++ ) {
+                int min = arr[l];
+                int max = arr[l];
+                for( int i = l+1; i < r; i++ ) {
+                
+                    if ( max < arr[i] ) {
+                        max = arr[i];
+                    }
+                    if ( min > arr[i] ) {
+                        min = arr[i];
+                    }
+                }
+                long long tmp = min * max;
+                if ( maxResult < tmp ) {
+                    maxResult = tmp;
+                }
+            }
+        }
+
+        printf("%d\n", maxResult);
 
     }
 
